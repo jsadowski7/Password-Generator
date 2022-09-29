f2b622db1c9b9e57d6dc088e48e24bb3b1ab641a
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-09-29 18:15:52 -0500

    Added 'if' statement for prompts to script.js file

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Set variables for prompt questions 
+var askNumber;
+var enter;
+var askCharacter;
+var askUppercase;
+var askLowercase;
+
+//checks prompts
 function generatePassword() {
-  //checks prompts
-
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  enter = parseInt(prompt("How many characters would you like your password?"))
+  if (!enter) {
+    // If no input value 
+    alert("Please enter a value");
+  } else if (enter < 8 || enter > 128) {
+    enter = parseInt(prompt("Enter value between 8 and 128"));
+  } else {
+    // Prompt questions
+    askNumber = confirm("Will this contain numbers?");
+    askCharacter = confirm("Will this contain special characters?");
+    askUppercase = confirm("Will this contain Uppercase letters?");
+    askLowercase = confirm("Will this contain Lowercase letters?");
+  }
+// If statement for if no prompts are selected 
+  if (!askNumber && !askCharacter && !askUppercase && !askLowercase) {
+    choices = alert("You must choose a criteria");
+  }
   //generate random string
-
   return randomString
 }
 
