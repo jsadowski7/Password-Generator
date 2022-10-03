0dd16c9e4ce1a5858025d22c08343d3dc5166887
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-10-03 17:12:36 -0500

    Added confirmed prompt options to script.js file

--- script.js
+++ script.js
@@ -2,15 +2,26 @@
 var generateBtn = document.querySelector("#generate");
 
 // Set variables for prompt questions 
-var askNumber;
 var enter;
+var askNumber;
 var askCharacter;
 var askUppercase;
 var askLowercase;
+var choices;
+
+var character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "_", "[", "]", "{", "}", ";", ":", "?", "/", ".", ",", "<", ">", "`", "~"];
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Function that changes letters to Uppercase
+var toUpper = function(x) {
+  return x.toUpperCase();
+}
+// Variable for uppercase letters from function used above
+var alpha2 = alpha.map(toUpper);
+
 
 //checks prompts
 function generatePassword() {
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   enter = parseInt(prompt("How many characters would you like your password?"))
   if (!enter) {
     // If no input value 
@@ -28,15 +39,52 @@ function generatePassword() {
   if (!askNumber && !askCharacter && !askUppercase && !askLowercase) {
     choices = alert("You must choose a criteria");
   }
-  //generate random string
-  return randomString
+  else if (askNumber && askCharacter && askUppercase && askLowercase) {
+    choices = character.concat(number, alpha, alpha2);
+  }
+  // Else if statements for 3 confirmed prompts and 1 Cancelled 
+  else if (askNumber && askCharacter && askUppercase) {
+    choices = character.concat(number, alpha2);
+  }
+  else if (askNumber && askCharacter && askLowercase) {
+    choices = character.concat(number, alpha);
+  }
+  else if (askCharacter && askLowercase && askUppercase) {
+    choices = character.concat(alpha, alpha2);
+  }
+  else if (askNumber && askLowercase && askUppercase) {
+    choices = number.concat(alpha, alpha2);
+  }
+  // Else if statements for 2 confirmed prompts and 2 cancelled
+  else if (askCharacter && askNumber) {
+    choices = character.concat(number);
+  }
+  else if (askCharacter && askLowercase) {
+    choices = character.concat(alpha);
+  }
+  else if (askCharacter && askUppercase) {
+    choices = character.concat(alpha2);
+  }
+// Else if statements for 1 confirmed prompt
+  else if (askCharacter) {
+    choices = character;
+  }
+  else if (askNumber) {
+    choices = number;
+  }
+  else if (askLowercase) {
+    choices = alpha;
+  }
+  else if (askUppercase) {
+    choices = alpha2;
+  }
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  // 'For' Statement?
   passwordText.value = password;
 
 }
